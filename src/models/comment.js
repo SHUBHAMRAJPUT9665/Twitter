67711f3261ce59c0dbbb0ef306099491fde2c1e3
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-05-31 07:31:30 +0530

    comment model

--- src/models/comment.js
+++ src/models/comment.js
@@ -4,9 +4,22 @@ const commentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    userEmail: {
-        type: String
+    userId: {
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
     },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ["Tweet", "Comment"],
+      },
+      commentable: {
+        type:Schema.Types.ObjectId,
+        required: true,
+        refPath: "onModel",
+      },
+      
 }, {timestamps: true});
 
 export default  Comment = mongoose.model('Comment', commentSchema);
