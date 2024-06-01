d0641e4cd178e93ad970f1dc56f8d9852c55a1a4
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-06-01 06:03:54 +0530

    content

--- src/controllers/comment-controller.js
+++ src/controllers/comment-controller.js
@@ -1,9 +1,8 @@
 import CommentService from "../services/comment-service.js";
-
 const commentService = new CommentService();
 
 export const createComment = async (req, res) => {
-
+ // content in body
     try {
         const response = await commentService.create(req.query.modelId, req.body.modelType, req.body.userId, req.body.content);
         return res.status(201).json({
--- src/repository/comment-repository.js
+++ src/repository/comment-repository.js
@@ -2,20 +2,20 @@ import CrudRepository from "./crud-repository.js";
 import Comment from "../models/comment.js";
 
 class CommentRespository extends CrudRepository {
-    constructor(){
-        super(Comment)
-    }
+  constructor() {
+    super(Comment);
+  }
 
-    async create(data) {
-        try {
-          const result = await this.model.create(data);
-          return result;
-        } catch (error) {
-          console.log("Something went wrong in crud repo creaate");
-    
-          throw error;
-        }
-      }
+  async create(data) {
+    try {
+      const result = await this.model.create(data);
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in crud repo creaate");
+
+      throw error;
+    }
+  }
 }
 
-export default CommentRespository
+export default CommentRespository;
