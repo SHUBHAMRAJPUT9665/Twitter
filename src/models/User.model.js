af145fc347069564f04a1474b1f74c94cec13e95
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-06-02 06:05:04 +0530

    Add: password encrypt

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import  jwt  from "jsonwebtoken";
+import bcrypt from 'bcrypt'
 const userSchema = new Schema({
     email: {
         type: String,
@@ -16,6 +17,16 @@ const userSchema = new Schema({
     }
 }, {timestamps: true});
 
+UserSchema.pre("save", async function (next){
+    if(!this.isModified('password')) return next();
+ 
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+ })
+ 
+ UserSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password)
+ }
 const User = mongoose.model('User', userSchema);
 
 export default User;
