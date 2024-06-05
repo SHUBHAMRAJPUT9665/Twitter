a16ae41a5d29e4267306e08d8e91cb5e1788acd8
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-06-05 09:47:21 +0530

    email sending

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,12 +1,39 @@
 import UserService from "../services/user-service.js";
 const userService = new UserService();
 
+import  nodemailer from "nodemailer"
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "shubhamrajput9665@gmail.com",
+    pass: "vjhb uctw dmih kczy",
+  },
+});
+
 const signup = async (req, res) => {
   try {
     const response = await userService.signup({
       email: req.body.email,
       password: req.body.password,
       name: req.body.name,
+    });
+
+
+    var mailOptions = {
+      from: 'youremail@gmail.com',
+      to: response.email,
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!'
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
     });
 
     console.log(response);
